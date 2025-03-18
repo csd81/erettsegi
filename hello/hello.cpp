ee87b653c6f5da9036a9c21e306853b7b7049bb8
csd81 <48775821+csd81@users.noreply.github.com>
2025-03-18 12:05:30 +0000

    +

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -1,69 +1,27 @@
-// let's build a simple chess game
-// we will have a board of 8x8 - we will use a 2D array
-char board[8][8] = {
-    {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
-    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-    {'', '', '', '', '', '', '', ''},
-    {'', '', '', '', '', '', '', ''},
-    {'', '', '', '', '', '', '', ''},
-    {'', '', '', '', '', '', '', ''},
-    {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-    {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}
-};
+#include <iostream>
+#include <vector>
+#include <algorithm> // required for std::fill
 
-void printBoard() {
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            cout << board[i][j] << " ";
-        }
-        cout << endl;
+int main() {
+    std::vector<int> vec(5);
+    std::fill(vec.begin(), vec.end(), 10);
+    for (int i = 0; i < vec.size(); i++) {
+        std::cout << vec[i] << " ";
     }
+    return 0;
 }
-legalMovesPawn = {
-    {1, 0},
-    {2, 0},
-    {1, 1},
-    {1, -1}
-};
+/*
+The output of the program is:
+10 10 10 10 10
+Explanation: The std::fill function is used to fill a range of elements in a container with a specific value. 
+In this case, the vector vec is filled with the value 10 using 
+std::fill(vec.begin(), vec.end(), 10). 
+The for loop then prints out each element of the vector, 
+resulting in the output 10 10 10 10 10. 
+This is because all elements in the vector have been filled with the value 10.
+The std::fill function takes three arguments:
+1. The beginning of the range to fill (in this case, vec.begin()).
+2. The end of the range to fill (in this case, vec.end()).
+3. The value to fill the range with (in this case, 10).
+*/
 
-// we will have 2 players
-// each player will have 16 pieces
-// 8 pawns, 2 rooks, 2 knights, 2 bishops, 1 queen, 1 king
-legalMovesRook = {
-    {1, 0},
-    {2, 0},
-    {3, 0},
-    {4, 0},
-    {5, 0},
-    {6, 0},
-    {7, 0},
-    {8, 0},
-    {0, 1},
-    {0, 2},
-    {0, 3},
-    {0, 4},
-    {0, 5},
-    {0, 6},
-    {0, 7},
-    {0, 8}
-};
-
-// each piece will have a color
-// the board will have 2 colors
-// the pieces will move according to their rules
-// pawns can move 1 or 2 squares forward, 1 square diagonally to capture
-// rooks can move any number of squares horizontally or vertically
-// knights can move in an L shape (2 squares in one direction, 1 square in the other)
-// bishops can move diagonally any number of squares (like the rook)
-// queens can move any number of squares in any direction (like the rook and the bishop)
-// kings can move 1 square in any direction
-// the game will start with the white player
-
-// the game will end when one of the kings is captured
-// the game will end when one of the kings is in checkmate
-// the game will end when one of the players resigns
-// the game will end when there is a stalemate
-// the game will end when there is a draw
-// the game will end when there is a 3-fold repetition
-// the game will end when there is a 50-move rule
-// the game will end when there is insufficient material
--- obj/Debug/net8.0/erettsegi.AssemblyInfo.cs
+++ obj/Debug/net8.0/erettsegi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("erettsegi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cc8db9b58c9c50349e5aa3db03a4c5de8b14251")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1fbbe6cea3f2ea429ed22d76ba4b80aba1b693d")]
 [assembly: System.Reflection.AssemblyProductAttribute("erettsegi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("erettsegi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
