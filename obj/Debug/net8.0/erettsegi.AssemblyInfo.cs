2e47758ddab4f0eecff5f71d2d32c52671b94482
csd81 <48775821+csd81@users.noreply.github.com>
2025-03-19 12:44:19 +0000

    book.md -> 123.md

--- obj/Debug/net8.0/erettsegi.AssemblyInfo.cs
+++ obj/Debug/net8.0/erettsegi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("erettsegi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e7556228c9d3dd3672578603a0d01ab0b20513d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c40c2348b67012cff58a894884b4732bf3f83190")]
 [assembly: System.Reflection.AssemblyProductAttribute("erettsegi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("erettsegi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
